25c3e5ed289a7b4424ce8b19202fc1bf60f92ef9
Onimai！ <93068569+Onimaimai@users.noreply.github.com>
2024-10-20 16:13:12 +0800

    添加元数据配置

--- nonebot_plugin_voicemusic/__init__.py
+++ nonebot_plugin_voicemusic/__init__.py
@@ -14,7 +14,7 @@ __plugin_meta__ = PluginMetadata(
     description="用语音收听点歌",
     usage="点歌 歌名 可选歌手",
     type="application",
-    homepage="https://github.com/Onimaimai/nonebot-plugin-voicemusic",
+    config=Config, homepage="https://github.com/Onimaimai/nonebot-plugin-voicemusic",
     supported_adapters={"~onebot.v11"},
 )
 
