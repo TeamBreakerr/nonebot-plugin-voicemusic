c494b4966daddb556eab1f5a72e12a1c6929d833
Onimai！ <93068569+Onimaimai@users.noreply.github.com>
2024-12-19 11:35:33 +0000

    0

--- nonebot_plugin_voicemusic/config.py
+++ nonebot_plugin_voicemusic/config.py
@@ -1,13 +1,5 @@
-from pydantic import BaseModel, validator
+from pydantic import BaseModel
 
 class Config(BaseModel):
     uin: str = ""
     skey: str = ""
-
-    @validator("uin", pre=True)
-    def str_uin(cls, v):
-        return str(v)  # 强制转换为字符串
-
-    @validator("skey", pre=True)
-    def str_skey(cls, v):
-        return str(v)  # 强制转换为字符串
